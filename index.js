f18b748417c212acee4bc37c71695473148b52b0
Ali Majid <100522435+Ali161725@users.noreply.github.com>
2023-04-11 04:32:17 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const controller = require('./controllers/index.js')
+const server = require('./routes/user.js')
+const database = require('./config/dbConfig.js')
+const bodyParser = require("body-parser");
+
+database.authenticate()
+  .then(() => {
+    console.log('Berhasil terhubung');
+  })
+  .catch(err => {
+    console.error(`Gagal terhubung : ${err}`);
+  });
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+app.use(server)
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
---
+++ routes/user.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const server = express.Router()
+const controller = require('../controllers/index.js')
+
+server.get('/dokumen', controller.doc.cekDokumen)
+server.post('/create', controller.doc.buatDokumen)
+// server.post('/edit', controller.doc.editDokumen)
+
+server.post('/register', controller.user.register)
+// server.post('/login', controller.user.login)
+
+module.exports = server
